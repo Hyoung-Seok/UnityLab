513117afe759d223595fd7436475040e2a685a87
Hyoung-Seok <dkavmdk98@gmail.com>
2026-05-06 19:10:45 +0900

    [Pool] 플레이어 회전 코드 작성

--- Assets/2_Scripts/ObjectPool/Player.cs
+++ Assets/2_Scripts/ObjectPool/Player.cs
@@ -1,10 +1,32 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    private Camera _camera;
+    private Plane _aimPlane;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+        _aimPlane = new Plane(Vector3.up, transform.position.y);
+    }
+
     private void Update()
     {
-        
+        var ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (_aimPlane.Raycast(ray, out var dist))
+        {
+            var hit = ray.GetPoint(dist);
+            var dir = hit - transform.position;
+            dir.y = 0;
+
+            if (dir.sqrMagnitude > 0.001f)
+            {
+                transform.rotation = Quaternion.LookRotation(dir);
+            }
+        }
     }
 }
