32c2f1e41cb1f1a5e55c9e8730835aead4881761
Hyoung-Seok <dkavmdk98@gmail.com>
2026-05-06 20:55:14 +0900

    [Pool] 플레이어 회전 코드 작성

--- Assets/2_Scripts/ObjectPool/Player.cs
+++ Assets/2_Scripts/ObjectPool/Player.cs
@@ -4,20 +4,26 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Weapon equippedWeapon;
+
+    private GameInput _gameInput;
     private Camera _camera;
     private Plane _aimPlane;
 
-    private void Start()
+    private void Awake()
     {
         _camera = Camera.main;
         _aimPlane = new Plane(Vector3.up, transform.position.y);
+        
+        _gameInput = new GameInput();
+        _gameInput.Enable();
     }
 
     private void Update()
     {
         var ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-        if (_aimPlane.Raycast(ray, out var dist))
+        if (_aimPlane.Raycast(ray, out var dist) == true)
         {
             var hit = ray.GetPoint(dist);
             var dir = hit - transform.position;
@@ -28,5 +34,22 @@ public class Player : MonoBehaviour
                 transform.rotation = Quaternion.LookRotation(dir);
             }
         }
+    }
+
+    private void OnEnable()
+    {
+        _gameInput.Player.Fire.started += OnStartFire;
+        _gameInput.Player.Fire.performed += OnStartFire;
+    }
+
+    private void OnDisable()
+    {
+        _gameInput.Player.Fire.started -= OnStartFire;
+        _gameInput.Player.Fire.performed -= OnStartFire;
+    }
+
+    private void OnStartFire(InputAction.CallbackContext _)
+    {
+        equippedWeapon.Fire();
     }
 }
