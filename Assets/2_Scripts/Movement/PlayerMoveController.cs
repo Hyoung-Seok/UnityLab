e58b559c6461c2b7eaeae8fdf5845ee8aac8d301
Hyoung-Seok <dkavmdk98@gmail.com>
2026-06-20 00:38:14 +0900

    [M] rb - MovePosition()을 사용해 이동 코드 구현

--- Assets/2_Scripts/Movement/PlayerMoveController.cs
+++ Assets/2_Scripts/Movement/PlayerMoveController.cs
@@ -3,19 +3,32 @@ using UnityEngine;
 
 public class PlayerMoveController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed = 10.0f;
+
+    private Vector3 _dir;
     
     private void Update()
     {
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
         
-        TransformMove(h, v);
+        _dir = new Vector3(h, 0, v).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        RigidbodyMovePosition();
     }
 
     private void TransformMove(float h, float v)
     {
         var dir = new Vector3(h, 0, v).normalized;
         transform.position += dir * (speed * Time.deltaTime);
+    }
+
+    private void RigidbodyMovePosition()
+    {
+        rb.MovePosition(rb.position + _dir * (speed * Time.fixedDeltaTime));
     }
 }
