4b193adc59621b4e226a59372d81851be5d79ceb
Hyoung-Seok <dkavmdk98@gmail.com>
2026-06-22 18:31:06 +0900

    [M] CharacterController로 움직임 구현

--- Assets/2_Scripts/Movement/PlayerMoveController.cs
+++ Assets/2_Scripts/Movement/PlayerMoveController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 public class PlayerMoveController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private CharacterController cc;
     [SerializeField] private float speed = 10.0f;
 
     private Vector3 _dir;
+    private Vector3 _velocity = Vector3.zero;
+    private float _gravity = -9.81f;
     
     private void Update()
     {
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
+        var input = new Vector3(h, 0, v);
         
-        _dir = new Vector3(h, 0, v).normalized;
+        // 수평 이동
+        var horizontal = input * speed;
+        
+        // 중력 누적
+        if(cc.isGrounded && _velocity.y < 0f)
+            _velocity.y = -2f;
+        else
+            _velocity.y += _gravity * Time.deltaTime;
+
+        cc.Move((horizontal + _velocity) * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(_dir * speed);
+        //rb.AddForce(_dir * speed);
     }
 
     private void TransformMove(float h, float v)
